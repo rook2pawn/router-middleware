3274c46d0de4f5fcbe672b7f9e3841cd1682f970
David Wee <rook2pawn@gmail.com>
2015-12-31 06:25:00 -0800

    separating index, more

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 var methods = require('methods')
-var url = require('url')
 var statuscodes = require('./lib/statuscodes')
 var handle = require('./lib/handle')
 
 methods.forEach(function(method) {
   handle.setMethod(method)
 })
-
 statuscodes.forEach(function(code) {
   handle.setCode(code)
 })
-
-handle.setstatusroutes(statusroutes)
 module.exports = exports = handle
