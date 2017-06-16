395de19b53a30495d44e24201cc0c250cda83ac9
rook2pawn <rook2pawn@gmail.com>
2017-06-16 00:28:55 -0700

    added test

--- test/test-post.js
+++ test/test-post.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var qs = require('querystring');
 
 
-test('test post', function(t) {
+test.skip('test post', function(t) {
   t.plan(2)
   var app = router()
   app.post('/user',function(req,res) {
@@ -21,5 +21,26 @@ test('test post', function(t) {
   .end(function(err,res) {
     console.log(res.text);
     t.equal(res.text, 'Manny')
+  })
+});
+
+test('test post', function(t) {
+
+  t.plan(3);
+  var app = router()
+  app.post('/user/:userId/notify',function(req,res) {
+    t.ok(req.body.message);
+    t.ok(req.params.userId);
+    var username = req.params.userId;
+    var message = req.body.message;
+    res.write(username.concat(':').concat(message))
+    res.end()
+  })
+  var x = request(app);
+  x
+  .post('/user/manny/notify')
+  .send({ message: 'Hello!' })
+  .end(function(err,res) {
+    t.equal(res.text, 'manny:Hello!')
   })
 })
